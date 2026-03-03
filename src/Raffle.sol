534eb930d130965b835b67109575634ad8b09915
olayinkasanusi <sanusiolayinka7@gmail.com>
2026-03-03 09:12:26 +0100

    Init commit for sample raffle smart contract

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title A sample Raffle comtract
+ * @author Sanusi Olayinka
+ * @notice This contract is for creating a simple Raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+    function enterRaffle() public {}
+    function pickWinner() public {}
+}
